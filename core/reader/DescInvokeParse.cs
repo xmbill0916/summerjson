a441c7b685daae861eb41bc695ae2bc4ce7f3b6f
xmbill <68999314+xmbill0916@users.noreply.github.com>
2021-10-10 22:19:37 +0800

    Add files via upload

---
+++ core/reader/DescInvokeParse.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Copyright(C) 2021, 2031 xmbill0916 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using com.xmbill.json.api;
+using System;
+using System.Collections.Generic;
+
+namespace com.xmbill.json.core.reader
+{
+    public class DescInvokeParse : IJsonParseInvoke
+    {
+        private JsonPathDesc root;
+        private JsonPathDesc currentDesc;
+        private List<JsonPathDesc> stackNodeDesc = new List<JsonPathDesc>(32);
+
+        public static IJsonParseInvoke newInstance(JsonPathDesc root)
+        {
+            DescInvokeParse descInvokeValue = new DescInvokeParse();
+            descInvokeValue.setJsonPathDesc(root);
+            return descInvokeValue;
+        }
+
+        public void setJsonPathDesc(JsonPathDesc root)
+        {
+            this.root = root;
+        }
+
+        public object beforeParseObject(object parentObj, string parentKey, List<string> descPath)
+        { 
+            if ("".Equals(parentKey))
+            {
+                if (descPath.Count == 1)
+                {
+                    if (root.GetNodeDesc() != null)
+                    {
+                        currentDesc = root;
+                    }
+                    else
+                    {
+                        currentDesc = null;
+                    }
+                }
+                else
+                    currentDesc = null;
+            }
+            else
+            {
+                if (currentDesc != null)
+                {
+                    JsonPathDesc currentDescTmp = currentDesc.GetChild(parentKey);
+                    if ((!"*".Equals(parentKey)) && (currentDescTmp == null))
+                        currentDescTmp = currentDesc.GetChild("*");
+                    currentDesc = currentDescTmp;
+                }
+            }
+            stackNodeDesc.Add(currentDesc);
+            if (currentDesc == null)
+                return new Dictionary<string, object>();
+            else
+                return currentDesc.NewInstance(parentObj);
+        }
+
+        public void setObjectKeyValue(object obj, JsonType jsonType, string key, object value)
+        {
+            if (currentDesc != null)
+                currentDesc.Set(obj, jsonType, key, value);
+            else
+                ((Dictionary<string, object>)obj).Add(key, value);
+        }
+
+        public void afterParseObject(object parentObj, string parentKey, object obj, List<string> descPath)
+        {
+            int index = stackNodeDesc.Count - 1;
+            stackNodeDesc.RemoveAt(index);
+            if (index > 0)
+                currentDesc = stackNodeDesc[index - 1];
+            else
+                currentDesc = null;
+        }
+
+        public object beforeParseArray(object parentObj, string parentKey, List<string> descPath)
+        {
+            if ("".Equals(parentKey))
+            {
+                if (descPath.Count == 1)
+                {
+                    if (root.GetNodeDesc() != null)
+                    {
+                        currentDesc = root;
+                    }
+                    else
+                    {
+                        currentDesc = null;
+                    }
+                }
+                else
+                    currentDesc = null;
+            }
+            else
+            {
+                if (currentDesc != null)
+                {
+                    currentDesc = currentDesc.GetChild(parentKey);
+                }
+            }
+            stackNodeDesc.Add(currentDesc);
+            if (currentDesc == null)
+                return new List<object>();
+            else
+                return currentDesc.NewInstance(parentObj);
+        }
+
+        public void setArrayValue(object list, JsonType jsonType, int index, object value)
+        {
+            if (currentDesc != null)
+                currentDesc.Set(list, jsonType, index, value);
+            else
+                ((List<object>)list).Add(value);
+        }
+
+        public void afterParseArray(object parentObj, string parentKey, object ary, List<string> descPath)
+        {
+            int index = stackNodeDesc.Count - 1;
+            stackNodeDesc.RemoveAt(index);
+            if (index > 0)
+                currentDesc = stackNodeDesc[index - 1];
+            else
+                currentDesc = null;
+        }
+
+    }
+}
---
+++ core/reader/JsonParserObject.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * Copyright(C) 2021, 2031 xmbill0916 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using com.xmbill.json.api;
+using System.Collections.Generic; 
+
+namespace com.xmbill.json.core.reader
+{
+    public class JsonParserObject:JsonParser
+    {
+        /**
+   * 解析对象开始
+   *
+   * @param jsonParameter
+   */
+        public static void parseObjectStart(JsonParameter jsonParameter)
+        {
+            jsonParameter.index++;
+            skipSpace(jsonParameter);
+        }
+
+        /**
+         * 解析对象结束
+         * 到下一个起启点
+         *
+         * @param jsonParameter
+         */
+        public static void parseObjectEnd(JsonParameter jsonParameter)
+        {
+            jsonParameter.index++;
+            skipSpaceOfEnd(jsonParameter);
+        }
+
+        public static object parseObject(JsonParameter jsonParameter, IJsonParseInvoke jsonValueInvoke)
+        {
+            return parseObject(null,"", new List<string>(), jsonParameter, jsonValueInvoke);
+        }
+
+        public static object parseObject(object parentObj, string parentKey, List<string> descPath, JsonParameter jsonParameter, IJsonParseInvoke jsonValueInvoke)
+        {
+            parseObjectStart(jsonParameter);
+            bool isEnd = isObjectEnd(jsonParameter);
+            descPath.Add(parentKey);
+            object obj = jsonValueInvoke.beforeParseObject(parentObj,parentKey, descPath);
+            if (!isEnd)
+            {
+                string key;
+                object value = null;
+                int ijt;
+                do
+                {
+                    key = getObjectKeyAndSkipToValue(jsonParameter);
+                    ijt = getInternalJsonType(jsonParameter);
+                    switch (ijt)
+                    {
+                        case 1:
+                            {
+                                value = parseObject(obj,key, descPath, jsonParameter, jsonValueInvoke);
+                                break;
+                            }
+                        case 2:
+                            {
+                                value = JsonParserArray.parseArray(obj,key, descPath, jsonParameter, jsonValueInvoke);
+                                break;
+                            }
+                        case 3:
+                            {
+                                value = getStringOfString(jsonParameter);
+                                break;
+                            }
+                        case 4:
+                            {
+                                value = getNumberOfString(jsonParameter);
+                                break;
+                            }
+                        case 5:
+                            {
+                                getTrueValue(jsonParameter);
+                                value = true;
+                                break;
+                            }
+                        case 6:
+                            {
+                                getFalseValue(jsonParameter);
+                                value = false;
+                                break;
+                            }
+                        case 7:
+                            {
+                                getNullValue(jsonParameter);
+                                value = null;
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                    jsonValueInvoke.setObjectKeyValue(obj, jsonTypes[ijt], key, value);
+                    if (hasNonObjectNextNode(jsonParameter))
+                        break;
+                } while (isNonEnd(jsonParameter));
+            }
+            parseObjectEnd(jsonParameter);
+            jsonValueInvoke.afterParseObject(parentObj, parentKey, obj, descPath);
+            descPath.RemoveAt(descPath.Count - 1);
+            return obj;
+
+        }
+    }
+}
